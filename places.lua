a3d108bfc054c663828758e9dcf90a7dcb7e3400
whoIsN3lly <126232383+whoIsN3lly@users.noreply.github.com>
2023-06-20 19:42:20 +0600

    sw33t pl2c3s

---
+++ places.lua
@@ -0,0 +1,12 @@
+local places = {}
+local table = {
+  ["aut"] = 6846458508,
+  ["baseplate"] = 4483381587,
+  ["yba"] = 2809202155,
+}
+
+function places.return()
+  return places.table
+end
+
+return places
